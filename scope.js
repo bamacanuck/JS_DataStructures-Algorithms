decff11d1253f15deac02a182fd8336306dbc217
S.R. Scott <shaneradio@gmail.com>
2019-05-29 17:04:20 -0400

    scope.js...

--- scope.js
+++ scope.js
@@ -1,3 +1,13 @@
 let myVariable = 'global';
 let myOtherVariable = 'global';
 
+function myFunction() {
+    let myVariable = 'local';
+    return myVariable;
+}
+
+function myOtherFunction() {
+    let myOtherVariable = 'local';
+    return myOtherVariable;
+}
+
