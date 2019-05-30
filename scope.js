eaaee4d500b7d13653e5b3ebc744dc5279dd6b3b
S.R. Scott <shaneradio@gmail.com>
2019-05-30 18:26:20 -0400

    altered scope.js

--- scope.js
+++ scope.js
@@ -12,8 +12,12 @@ function myOtherFunction() {
 }
 
 console.log(myVariable); // 1
+
 console.log(myFunction()); // 2
 
+
 console.log(myOtherVariable); // 3
+
 console.log(myOtherFunction()); // 4
+
 console.log(myOtherVariable); // 5
